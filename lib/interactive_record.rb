e3d1762f6e6493e2a6e3981f2f81c5ac6380d69e
Allison Howard <alliemhow@gmail.com>
2019-01-09 22:46:39 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,14 +20,16 @@ class InteractiveRecord
     column_names.compact
   end
 
-  self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
-  end
-
   def intialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
     end
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
 end
