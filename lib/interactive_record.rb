6250a29774202890d7681991a9ebb5ef92e0de53
Allison Howard <alliemhow@gmail.com>
2019-01-09 23:29:03 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,7 +55,8 @@ class InteractiveRecord
 
   def self.find_by(attribute_hash)
     value = attribute_hash.values.first
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first}= #{value}"
+    formatted_value = value.class
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
     DB[:conn].execute(sql)
   end
 
